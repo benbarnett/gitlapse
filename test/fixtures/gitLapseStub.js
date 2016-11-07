c377578b756ac5041ffd545ab6d971655316635f
Ben Barnett <ben@brushfiredesign.net>
2016-11-07 11:56:45 +0000

    Refactor exectuable to use a more standard module pattern
    Allows for easier unit testing by avoiding the need to actually
    execute a binary.

---
+++ test/fixtures/gitLapseStub.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class GitLapseStub {
+  constructor(config) {
+    this.config = config;
+  }
+
+  go() {
+    return this;
+  }
+}
+
+module.exports = GitLapseStub;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@
 const FIXTURE_REPO = __dirname + '/fixtures/gitlapse-test';
 
 var expect = require('chai').expect;
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+
 var git = require('simple-git')(FIXTURE_REPO);
 var testConfig = {
   'scripts': {
@@ -61,6 +64,66 @@ describe('Walking the git repository', function() {
           done();
         });
       });
+    });
+  });
+});
+
+describe('The executable passes in the config', function() {
+  beforeEach(function() {
+    var name = require.resolve('commander');
+    delete require.cache[name];
+
+    this.sinon = sinon.sandbox.create();
+  });
+
+  afterEach(function(){
+    this.sinon.restore();
+  });
+
+  // Fake running the gitlapse from the command line
+  function runGitLapseExecutableStub(args) {
+    var gitLapseStub = require('./fixtures/gitLapseStub');
+    var gitLapseExecutable = proxyquire('../lib/gitLapseExecutable', {' ../lib/index': gitLapseStub });
+    var actionSpy = this.sinon.spy(gitLapseExecutable, 'action');
+    var _args = ['node', '../bin/gitlapse'].concat(args);
+
+    gitLapseExecutable.init(_args);
+
+    return {
+      instantiatedGitLapse: actionSpy.returnValues[0],
+      gitLapseExecutable: gitLapseExecutable
+    };
+  }
+
+  it('passes through the commit range', function() {
+    var args = ['abcde..fghij'];
+    var instantiatedGitLapse = runGitLapseExecutableStub.call(this, args).instantiatedGitLapse;
+
+    expect(instantiatedGitLapse.config).to.have.property('start-revision', 'abcde');
+    expect(instantiatedGitLapse.config).to.have.property('end-revision', 'fghij');
+  });
+
+  it('passes through the steps option', function() {
+    var args = ['abcde..fghij', 15];
+    var instantiatedGitLapse = runGitLapseExecutableStub.call(this, args).instantiatedGitLapse;
+
+    expect(instantiatedGitLapse.config).to.have.property('steps', 15);
+  });
+
+  it('sets the path to the configuration file from option', function() {
+    var args = ['abcde..fghij', '--config', './test/fixtures/test-config.json'];
+    var gitLapseExecutable = runGitLapseExecutableStub.call(this, args).gitLapseExecutable;
+
+    expect(gitLapseExecutable.program().config).to.equal('./test/fixtures/test-config.json');
+  });
+
+  it('applies the options from the specified config file', function() {
+    var args = ['abcde..fghij', '--config', './test/fixtures/test-config.json'];
+    var instantiatedGitLapse = runGitLapseExecutableStub.call(this, args).instantiatedGitLapse;
+
+    expect(instantiatedGitLapse.config.scripts).to.eql({
+      "setup": "test/fixtures/setup.sh",
+      "server": "test/fixtures/server.sh"
     });
   });
 });
